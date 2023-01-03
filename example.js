126603795b6bfb5822f959faed041ee9dbea2f19
Tomer Y <44681727+TomerHawk@users.noreply.github.com>
2023-01-03 14:03:45 +0100

    Update example.js

--- example.js
+++ example.js
@@ -4,6 +4,17 @@ const options = {
     width: 700,
     height: 700,
     parentNode: document.querySelector('#meet'),
-    lang: 'de'
+    lang: 'de',
+    moderator: true,
+    userInfo: {
+        displayName: 'Banana Joe'
+    }
 };
 const api = new JitsiMeetExternalAPI(domain, options);
+api.addListener(incomingMessage, receiveMessage);
+
+// print message to console
+function receiveMessage(data) {
+    console.log("Here is my data: " + data.nick);
+}
+
